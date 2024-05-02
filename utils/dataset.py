dbb1d457e8d17a178a913143ec48bd0d84e56743
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-05-02 04:03:07 +0300

    use data loaders

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,21 +1,26 @@
-from sklearn.datasets import fetch_lfw_people
-from sklearn.model_selection import train_test_split
-import numpy as np
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader
 
-def load_dataset(test_size=0.2):
+# Define a transform to resize images to a uniform size
+transform = transforms.Compose([
+    transforms.Resize((37, 50)),  # Resize images to a uniform size
+    transforms.ToTensor()  # Convert images to tensors
+])
+
+def load_dataset(test_size=0.2, batch_size=32):
     # Load the LFW dataset
     lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
-    
-    # Resize images to the mean dimensions
-    X_resized = [resize_image(image, 50, 37) for image in lfw_people.images]
-    X_resized = np.array(X_resized)
+
+    # Preprocess images to have the same dimensions
+    X_resized = [transform(image) for image in lfw_people.images]
+    X_resized = torch.stack(X_resized)
 
     # Split dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X_resized, lfw_people.target, test_size=test_size)
 
-    return (X_train, X_test), (y_train, y_test)
+    # Create DataLoader objects
+    train_loader = DataLoader(list(zip(X_train, y_train)), batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(list(zip(X_test, y_test)), batch_size=batch_size, shuffle=False)
 
-def resize_image(image, width, height):
-    # Resize the image to the specified dimensions
-    from skimage.transform import resize
-    return resize(image, (height, width), mode='constant')
+    return train_loader, test_loader
