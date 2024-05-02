751d43162aa26658d0ebbd1f64f65bf3dd67ea4f
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-05-03 02:47:52 +0300

    tests

---
+++ tests.sh
@@ -0,0 +1,17 @@
+# Test AlexNet with all scenarios
+python main.py --model AlexNet --scenario 1
+python main.py --model AlexNet --scenario 2
+python main.py --model AlexNet --scenario 3
+python main.py --model AlexNet --scenario 4
+
+# Test ResNet with all scenarios
+python main.py --model ResNet --scenario 1
+python main.py --model ResNet --scenario 2
+python main.py --model ResNet --scenario 3
+python main.py --model ResNet --scenario 4
+
+# Test VGG with all scenarios
+python main.py --model VGG --scenario 1
+python main.py --model VGG --scenario 2
+python main.py --model VGG --scenario 3
+python main.py --model VGG --scenario 4
