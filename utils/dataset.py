95579c201244edd0792d6ae7849a21a000e5a6c9
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-05-02 04:01:19 +0300

    50x37

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,14 +5,9 @@ import numpy as np
 def load_dataset(test_size=0.2):
     # Load the LFW dataset
     lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
-
-    # Preprocess images to have the same dimensions
-    # Calculate the mean width and height
-    mean_width = int(np.mean([image.shape[1] for image in lfw_people.images]))
-    mean_height = int(np.mean([image.shape[0] for image in lfw_people.images]))
     
     # Resize images to the mean dimensions
-    X_resized = [resize_image(image, mean_width, mean_height) for image in lfw_people.images]
+    X_resized = [resize_image(image, 50, 37) for image in lfw_people.images]
     X_resized = np.array(X_resized)
 
     # Split dataset into training and testing sets
