c32420ee5a1ca39fd59b5bc5662d8173234b60a4
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-05-03 03:00:42 +0300

    remove max pooling at the output

--- models/alexnet.py
+++ models/alexnet.py
@@ -5,20 +5,20 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
+    nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Conv2d(64, 192, kernel_size=5, padding=2),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Conv2d(192, 384, kernel_size=3, padding=1),
+    nn.ReLU(inplace=True),
+    nn.Conv2d(384, 256, kernel_size=3, padding=1),
+    nn.ReLU(inplace=True),
+    nn.Conv2d(256, 256, kernel_size=3, padding=1),
+    nn.ReLU(inplace=True),
+    # nn.MaxPool2d(kernel_size=3, stride=2),  # Removed because of 256x0x0 output size
+)
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
             nn.Dropout(),
